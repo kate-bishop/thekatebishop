4212db8d2a86779925881e918b703cbe4b1d4039
Kate Perry <thekateperry@gmail.com>
2022-08-16 16:43:09 -0400

    Revert "Portfolio content"

---
+++ pages/resume.tsx
@@ -0,0 +1,11 @@
+import styles from '../styles/home.module.scss';
+import Wrapper from '../components/wrapper';
+
+export default function Resume() {
+    return (
+        <Wrapper>
+            <div className={styles.header}>
+            </div>
+        </Wrapper>
+    )
+}
