9386f46e373b41e548e86dd45f146093d04b47c5
Kate Perry <thekateperry@gmail.com>
2022-08-16 16:11:12 -0400

    bit 'o cleanup

--- utils/functions.js
+++ utils/functions.js
@@ -7,26 +7,19 @@ import html from 'remark-html'
 const portfolioDirectory = path.join(process.cwd(), 'portfolio')
 
 export function getSortedPortfolioData() {
-  // Get file names under /posts
   const fileNames = fs.readdirSync(portfolioDirectory)
   const allPortfolioData = fileNames.map(fileName => {
-    // Remove ".md" from file name to get id
     const id = fileName.replace(/\.mdx$/, '')
-
-    // Read markdown file as string
     const fullPath = path.join(portfolioDirectory, fileName)
     const fileContents = fs.readFileSync(fullPath, 'utf8')
-
-    // Use gray-matter to parse the post metadata section
     const matterResult = matter(fileContents)
 
-    // Combine the data with the id
     return {
       id,
       ...matterResult.data
     }
   })
-  // Sort posts by date
+
   return allPortfolioData.sort((a, b) => {
     if (a.date < b.date) {
       return 1
@@ -51,16 +44,15 @@ export async function getPortfolioData(id) {
   const fullPath = path.join(portfolioDirectory, `${id}.mdx`)
   const fileContents = fs.readFileSync(fullPath, 'utf8')
 
-  // Use gray-matter to parse the post metadata section
+  // gray-matter parses metadata
   const matterResult = matter(fileContents)
 
-  // Use remark to convert markdown into HTML string
+  // remark converts markdown > HTML string
   const processedContent = await remark()
     .use(html)
     .process(matterResult.content)
   const contentHtml = processedContent.toString()
 
-  // Combine the data with the id and contentHtml
   return {
     id,
     contentHtml,
