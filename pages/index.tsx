eda988dc37e9066c93134f0b2241f05b96cd4f59
Kate Perry <thekateperry@gmail.com>
2023-07-29 02:12:31 -0400

    add title to head tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,17 +2,23 @@ import Wrapper from '../components/wrapper';
 import Header from '../components/header';
 import About from '../components/about';
 import styles from '../styles/pages.module.scss';
+import Head from 'next/head';
 
 const Home: React.FC = () => {
   return (
-    <Wrapper>
-      <section>
-        <Header />
-      </section>
-      <section id="about" className={styles.about}>
-        <About />
-      </section>
-    </Wrapper>
+    <>
+      <Head>
+        <title>Kate Bishop</title>
+      </Head>
+      <Wrapper>
+        <section>
+          <Header />
+        </section>
+        <section id="about" className={styles.about}>
+          <About />
+        </section>
+      </Wrapper>
+    </>
   )
 }
 
