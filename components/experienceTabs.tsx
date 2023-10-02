b3d7e40c9cfbffd9d99e1ac89b368e1cecc0165b
Kate Perry <thekateperry@gmail.com>
2023-10-02 14:15:17 -0400

    move inline styles to scss

--- components/experienceTabs.tsx
+++ components/experienceTabs.tsx
@@ -1,24 +1,28 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { a11yProps } from '../utils/utils';
 import { Tabs, Tab, Typography } from '@mui/material';
 import TabPanel from './tabPanel';
 import { CSSTransition } from 'react-transition-group';
-import { palette, transitionTimeout } from '../utils/constants';
+import { transitionTimeout } from '../utils/constants';
 import { experience } from '../utils/strings';
 import { Experience } from '../utils/types';
+import { SmallScreenContext } from './wrapper';
+import styles from './experienceTabs.module.scss';
 
-// TODO move inline styles to css file, support for narrow screens
+// TODO support for narrow screens
 
 interface ExperienceTabBarProps {
     activeTab: number,
     handleChange: (event: React.SyntheticEvent, newValue: number) => void,
+    useSmallScreen: boolean,
 }
 const ExperienceTabBar: React.FC<ExperienceTabBarProps> = ({
     activeTab,
     handleChange,
+    useSmallScreen,
 }) => {
     return <Tabs
-        orientation="vertical"
+        orientation={useSmallScreen ? "horizontal" : "vertical"}
         variant="scrollable"
         value={activeTab}
         onChange={handleChange}
@@ -43,34 +47,31 @@ const ExperienceTabPanel: React.FC<ExperienceTabPanelProps> = ({
     index,
 }) => {
     return <TabPanel value={activeTab} index={index} key={index}>
-        <div style={{ height: '100%', overflow: 'auto' }}>
-            <div id="experience-title">
-                <Typography variant="h1">{experience.jobTitle}</Typography>
-                <Typography variant="subtitle1">{experience.location} | {experience.dateSpan}</Typography>
-            </div>
-            <div id="experience-company-details" style={{ marginTop: '1rem' }}>
-                <Typography variant="subtitle2">{experience.companyDescription}</Typography>
-            </div>
-            <div id="experience-position-details" style={{ marginTop: '1rem' }}>
-                <Typography variant="body1">{experience.description}</Typography>
-                {experience.keyProjects[0].projectName && <Typography variant='subtitle2' style={{ marginTop: '1rem' }}>Key Projects</Typography>}
-                {experience.keyProjects.map((project, pIndex) => {
-                    const projectHeader = project.projectName && <Typography variant='subtitle1'>{project.projectRole} | {project.projectName}</Typography>
-                    return <div key={`key-project-details-${index}-${pIndex}`}>
-                        {projectHeader}
-                        <ul>{project.bulletPoints.map((bullet, bIndex) => {
-                            return <li key={`key-project-details-${index}-${pIndex}-${bIndex}`}>
-                                <Typography variant='body1'>{bullet}</Typography>
-                            </li>
-                        })}
-                        </ul></div>;
-                })}
-            </div>
+        <Typography variant="h1">{experience.jobTitle}</Typography>
+        <Typography variant="subtitle1">{experience.location} | {experience.dateSpan}</Typography>
+        <div id="experience-company-details" className={styles.contentSection}>
+            <Typography variant="subtitle2">{experience.companyDescription}</Typography>
+        </div>
+        <div id="experience-position-details" className={styles.contentSection}>
+            <Typography variant="body1">{experience.description}</Typography>
+            {experience.keyProjects[0].projectName && <Typography variant='subtitle2' className={styles.contentSection}>Key Projects</Typography>}
+            {experience.keyProjects.map((project, pIndex) => {
+                const projectHeader = project.projectName && <Typography variant='subtitle1'>{project.projectRole} | {project.projectName}</Typography>
+                return <div key={`key-project-details-${index}-${pIndex}`}>
+                    {projectHeader}
+                    <ul>{project.bulletPoints.map((bullet, bIndex) => {
+                        return <li key={`key-project-details-${index}-${pIndex}-${bIndex}`}>
+                            <Typography variant='body1'>{bullet}</Typography>
+                        </li>
+                    })}
+                    </ul></div>;
+            })}
         </div>
     </TabPanel>;
 }
 
 const ExperienceTabs: React.FC = () => {
+    const useSmallScreen = useContext(SmallScreenContext);
     const [activeTab, setActiveTab] = useState(0);
     const [showContent, setShowContent] = useState(false);
 
@@ -88,11 +89,11 @@ const ExperienceTabs: React.FC = () => {
         classNames="content-left"
         unmountOnExit
         onEnter={() => setShowContent(true)}>
-        <div style={{ height: '100%', overflow: 'hidden', marginTop: '10vh' }}>
-            <div style={{ position: 'fixed', height: '100%', marginTop: '1.5rem' }}>
-                <ExperienceTabBar activeTab={activeTab} handleChange={handleChange} />
+        <div className={styles.tabContainer}>
+            <div className={styles.tabBar}>
+                <ExperienceTabBar activeTab={activeTab} handleChange={handleChange} useSmallScreen={useSmallScreen} />
             </div>
-            <div style={{ marginLeft: '15%', height: '100%', overflow: 'auto' }}>
+            <div className={styles.tabPanel}>
                 {experience.map((exp, index) => {
                     return <ExperienceTabPanel activeTab={activeTab} experience={exp} index={index} key={index} />
                 })}
