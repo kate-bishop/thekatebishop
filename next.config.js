e91be420bdc1b720a53b1b4c7f2232ce25ab8b3a
kate-bishop <itskatebishop@gmail.com>
2026-08-14 02:34:37 -0400

    cleanup

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
-const withMDX = require('@next/mdx')()
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Tell Next.js we have a custom turbopack setup
   turbopack: {
     // If you use specific webpack loaders, they go here now
   },
-  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
 }
 
-module.exports = withMDX(nextConfig)
+module.exports = nextConfig
