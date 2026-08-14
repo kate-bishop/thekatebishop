4fa5baa5592d309e5282c6b4bc6ca334296dcc1e
kate-bishop <itskatebishop@gmail.com>
2026-08-14 11:13:24 -0400

    bio layout

--- components/bio.tsx
+++ components/bio.tsx
@@ -1,8 +1,10 @@
 import { useState, useEffect } from 'react';
 import styles from './bio.module.scss';
+import utils from '../styles/utils.module.scss';
 import { transitionTimeout } from '../utils/constants';
 import { aboutMe } from '../utils/strings';
 import { CSSTransition } from 'react-transition-group';
+import PlaceholderPhoto from './placeholderPhoto';
 
 const Bio: React.FC = () => {
     const [showContent, setShowContent] = useState(false);
@@ -18,12 +20,21 @@ const Bio: React.FC = () => {
                 timeout={transitionTimeout}
                 classNames="content-grow">
                 <div className={styles.content}>
-                    <span className={styles.label}>the long version</span>
-                    <h1 className={styles.heading}>Hi, I&apos;m Kate! 👋</h1>
-                    <div className={styles.paragraphs}>
-                        <p className={styles.paragraph}>{aboutMe.bio.paragraph1}</p>
-                        <p className={styles.paragraph}>{aboutMe.bio.paragraph2}</p>
-                        <p className={styles.paragraph}>{aboutMe.bio.paragraph3}</p>
+                    <span className={`${styles.label} ${styles.highlight}`}>the long version</span>
+                    <div className={styles.row}>
+                        <div className={`${styles.frame} ${utils.handDrawnBox}`}>
+                            <h1 className={styles.heading}>Hi, I&apos;m Kate! 👋</h1>
+                            <div className={styles.paragraphs}>
+                                <p className={styles.paragraph}>{aboutMe.bio.paragraph1}</p>
+                                <p className={styles.paragraph}>{aboutMe.bio.paragraph2}</p>
+                                <p className={styles.paragraph}>{aboutMe.bio.paragraph3}</p>
+                            </div>
+                        </div>
+                        <div className={`${styles.photoCard} ${utils.tiltRight}`}>
+                            <span className={`${utils.tapeStrip} ${styles.tapeLeft}`} />
+                            <span className={`${utils.tapeStrip} ${styles.tapeRight}`} />
+                            <PlaceholderPhoto alt="Kate" label="photo" src="/images/kate-1.jpg" width={180} height={220} />
+                        </div>
                     </div>
                 </div>
             </CSSTransition>
