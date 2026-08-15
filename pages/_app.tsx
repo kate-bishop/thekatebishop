40c81b68609fee85e9915853e4a2a789004f2410
kate-bishop <itskatebishop@gmail.com>
2026-08-14 21:31:19 -0400

    speed insights

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import '../styles/globals.scss'
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <>
     <Component {...pageProps} />
     <Analytics />
+    <SpeedInsights/>
   </>
 }
 
